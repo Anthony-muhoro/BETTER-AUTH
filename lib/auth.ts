2dc9ac11e5aa5f6e0eae89323b34f0824f802296
Anthony-muhoro <anthonymuhoro2003@gmail.com>
2025-09-11 01:11:55 +0300

    .

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "./generated/prisma";
 import { nextCookies } from "better-auth/next-js";
-const prisma = new PrismaClient();
+import { prisma } from "./prisma";
+
 export const auth = betterAuth({
   // secret: process.env.BETTER_AUTH_SECRET!,
   // baseUrl: process.env.BETTER_AUTH_URL!,
